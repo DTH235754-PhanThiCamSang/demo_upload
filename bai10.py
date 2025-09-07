fdd838c4a22f72d94d1401cf6cc4a22cd5d8bcb1
Sang Phan <sang_dth235754@student.agu.edu.vn>
2025-09-07 14:52:55 +0700

    demo

--- bai10.py
+++ bai10.py
@@ -1,16 +1,25 @@
-max_cot = 11  # bề rộng tối đa (cứ để số lẻ sẽ đẹp hơn)
 
-# TỰ CHỌN độ rộng cho từng hàng (bao nhiêu cũng được)
-hang_widths = [1, 3, 7, 3, 5, 7, 11]
+max_cot = 11                   
+hang_widths = [1, 3, 7, 3, 5, 11]
 
-# in phần lá (theo danh sách trên)
+
+duoi_cao = 2                   
+khoang_trong_giua = 1           
+
+# ===== HÀM TIỆN ÍCH =====
+def print_center(s: str):
+    space = (max_cot - len(s)) // 2
+    print(" " * space + s)
+
+# ===== IN PHẦN LÁ =====
 for w in hang_widths:
-    space = (max_cot - w) // 2
-    print(" " * space + "*" * w)
+    dong = "*" * w
+    print_center(dong)
 
-# in thân cây (ví dụ: rộng 1 cột, cao 3 hàng)
-than_rong = 1
-than_cao = 3
-space = (max_cot - than_rong) // 2
-for _ in range(than_cao):
-    print(" " * space + "*" * than_rong)
+# ===== IN PHẦN ĐUÔI "* *" =====
+duoi_mot_hang = "*" + " " * khoang_trong_giua + "*"
+
+for _ in range(duoi_cao):
+    print_center(duoi_mot_hang)
+    
+    
